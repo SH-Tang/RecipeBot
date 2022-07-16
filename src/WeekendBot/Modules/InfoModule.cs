d3e1b2426331950b334348843d19dcbbaea744af
Dennis Tang <dennis.sht@gmail.com>
2022-07-16 17:15:33 +0200

    Updated license headers

--- src/WeekendBot/Modules/InfoModule.cs
+++ src/WeekendBot/Modules/InfoModule.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of WeekendBot.
+//
+// WeekendBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -16,7 +33,7 @@ namespace WeekendBot.Modules
         {
             DateTime currentTime = DateTime.Now;
             string message = ((currentTime.DayOfWeek & weekends) != 0)
-                ? "Ja het is weekend!"
+                ? "Ja het is weekend! XD"
                 : "Nee, dat is het niet :(";
             return ReplyAsync(message);
         }
