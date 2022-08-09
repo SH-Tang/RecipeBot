cf23a3153cab022b86e797c85553987f1ef4e072
Dennis Tang <dennis.sht@gmail.com>
2022-08-09 21:31:35 +0200

    Added tests to guarantee behaviour of the builder.

--- src/WeekendBot.Services/RecipeDataBuilder.cs
+++ src/WeekendBot.Services/RecipeDataBuilder.cs
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+using System;
 using Discord;
 using Discord.Common.Utils;
 using WeekendBot.Utils;
@@ -28,28 +29,53 @@ public class RecipeDataBuilder
 {
     private readonly RecipeData data;
 
-    public RecipeDataBuilder(AuthorData authorData, string recipeTitle, string recipeIngredients, string recipeSteps)
+    /// <summary>
+    /// Creates a new instance of <see cref="RecipeDataBuilder"/>.
+    /// </summary>
+    /// <param name="authorData">The <see cref="AuthorData"/>.</param>
+    /// <param name="recipeTitle">The title of the recipe.</param>
+    /// <param name="recipeIngredients">The ingredients of the recipe.</param>
+    /// <param name="cookingSteps">The cooking steps of the recipe.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="authorData"/> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="recipeTitle"/>, <see cref="recipeIngredients"/>
+    /// or <paramref name="cookingSteps"/> is <c>null</c> or consists of whitespaces.</exception>
+    public RecipeDataBuilder(AuthorData authorData, string recipeTitle, string recipeIngredients, string cookingSteps)
     {
-        data = new RecipeData(authorData, recipeTitle, recipeIngredients, recipeSteps);
+        data = new RecipeData(authorData, recipeTitle, recipeIngredients, cookingSteps);
     }
 
-    public RecipeDataBuilder AddImage(IAttachment attachment)
+    /// <summary>
+    /// Adds an attachment image url to the <see cref="RecipeData"/>.
+    /// </summary>
+    /// <param name="attachment">The <see cref="IAttachment"/> to add the information from.</param>
+    /// <returns>The <see cref="RecipeDataBuilder"/>.</returns>
+    /// <exception cref="ArgumentException">Thrown when the attachment image url could not be added.</exception>
+    public RecipeDataBuilder AddImage(IAttachment? attachment)
     {
-        attachment.IsNotNull(nameof(attachment));
-        attachment.IsValidArgument(x => x.IsImage(), "Attachment must be an image.", nameof(attachment));
+        if (attachment != null)
+        {
+            attachment.IsValidArgument(x => x.IsImage(), "Attachment must be an image.", nameof(attachment));
+            data.ImageUrl = attachment.Url;
+        }
 
-        data.ImageUrl = attachment.Url;
         return this;
     }
 
+    /// <summary>
+    /// Adds notes to the <see cref="RecipeData"/>.
+    /// </summary>
+    /// <param name="notes">The notes to add.</param>
+    /// <returns>The <see cref="RecipeDataBuilder"/>.</returns>
     public RecipeDataBuilder AddNotes(string notes)
     {
-        notes.IsNotNullOrWhiteSpaces(nameof(notes));
-
         data.AdditionalNotes = notes;
         return this;
     }
 
+    /// <summary>
+    /// Builds the <see cref="RecipeData"/>.
+    /// </summary>
+    /// <returns>A configured <see cref="RecipeData"/>.</returns>
     public RecipeData Build()
     {
         return data;
