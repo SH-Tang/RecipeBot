bce6daffdeb4a646bb66b1da5ae97a8991f5a62d
Siu Hin Tang <karabasf@gmail.com>
2022-08-17 23:36:28 +0200

    Added util to validate http or https urls

---
+++ src/WeekendBot.Domain/Utils/UrlValidationHelper.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of WeekendBot.
+//
+// WeekendBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+
+namespace WeekendBot.Domain.Utils;
+
+/// <summary>
+/// Helper class to validate urls.
+/// </summary>
+public static class UrlValidationHelper
+{
+    /// <summary>
+    /// Validates whether the <paramref name="url"/> is a valid http or https url.
+    /// </summary>
+    /// <param name="url">The url to validate.</param>
+    /// <returns><c>true</c> if <paramref name="url"/> is a valid http or https url, <c>false</c> otherwise.</returns>
+    public static bool IsValidUrl(string url)
+    {
+        if (string.IsNullOrWhiteSpace(url))
+        {
+            return false;
+        }
+
+        Uri? uriResult;
+        bool tryCreateResult = Uri.TryCreate(url, UriKind.Absolute, out uriResult);
+
+        return tryCreateResult && uriResult != null && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);                                          
+    }
+}
