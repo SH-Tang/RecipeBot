06812812b4394e356d9149de7fcafd171007959d
Dennis Tang <karabasf@gmail.com>
2022-08-18 22:19:06 +0200

    Added entity to hold data for an entire recipe.

---
+++ src/WeekendBot.Domain/Entities/RecipeDomainEntity.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of WeekendBot.
+//
+// WeekendBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WeekendBot.Utils;
+
+namespace WeekendBot.Domain.Entities;
+
+/// <summary>
+/// Entity containing data for a recipe.
+/// </summary>
+public class RecipeDomainEntity : ITotalCharacterLength
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="RecipeDomainEntity"/>.
+    /// </summary>
+    /// <param name="authorEntity">The author entity.</param>
+    /// <param name="recipeFieldEntities">The collection of recipe field entities.</param>
+    /// <param name="title">The title of the recipe.</param>
+    /// <exception cref="ArgumentNullException">Thrown when any parameter except <paramref name="title"/> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="title"/> is <c>null</c>, empty or consists of whitespaces.</exception>
+    internal RecipeDomainEntity(AuthorDomainEntity authorEntity, IEnumerable<RecipeFieldDomainEntity> recipeFieldEntities, string title)
+    {
+        authorEntity.IsNotNull(nameof(authorEntity));
+        authorEntity.IsNotNull(nameof(recipeFieldEntities));
+        title.IsNotNullOrWhiteSpaces(nameof(title));
+
+        AuthorEntity = authorEntity;
+        RecipeFieldEntities = recipeFieldEntities;
+        Title = title;
+    }
+
+    /// <summary>
+    /// Gets the title of the recipe.
+    /// </summary>
+    public string Title { get; }
+
+    /// <summary>
+    /// Gets the author information.
+    /// </summary>
+    public AuthorDomainEntity AuthorEntity { get; }
+
+    /// <summary>
+    /// Gets the fields the recipe consists of.
+    /// </summary>
+    public IEnumerable<RecipeFieldDomainEntity> RecipeFieldEntities { get; }
+
+    public int TotalLength
+    {
+        get
+        {
+            return Title.Length + AuthorEntity.TotalLength + RecipeFieldEntities.Sum(f => f.TotalLength);
+        }
+    }
+}
---
+++ test/WeekendBot.Domain.Test/Entities/RecipeDomainEntityTest.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of WeekendBot.
+//
+// WeekendBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WeekendBot.Domain.Entities;
+using WeekendBot.TestUtils;
+using Xunit;
+
+namespace WeekendBot.Domain.Test.Entities;
+
+public class RecipeDomainEntityTest
+{
+    [Theory]
+    [ClassData(typeof(EmptyOrNullStringValueGenerator))]
+    public void Entity_with_invalid_recipe_title_throws_exception(string invalidRecipeTitle)
+    {
+        // Setup
+        var random = new Random(21);
+
+        AuthorDomainEntity authorEntity = CreateAuthorEntity();
+        IEnumerable<RecipeFieldDomainEntity> recipeFieldEntities = new[]
+        {
+            CreateFieldEntity(random.Next()),
+            CreateFieldEntity(random.Next()),
+            CreateFieldEntity(random.Next())
+        };
+
+        // Call
+        Func<RecipeDomainEntity> call = () => new RecipeDomainEntity(authorEntity, recipeFieldEntities, invalidRecipeTitle);
+
+        // Assert
+        Assert.Throws<ArgumentException>(call);
+    }
+
+    [Theory]
+    [InlineData("recipeTitle")]
+    [InlineData("recipe title")]
+    [InlineData("recipe    title")]
+    [InlineData("     recipeTitle")]
+    [InlineData("recipeTitle     ")]
+    public void Entity_with_valid_data_returns_total_length_of_properties(string recipeTitle)
+    {
+        // Setup
+        var random = new Random(21);
+
+        AuthorDomainEntity authorEntity = CreateAuthorEntity();
+        IEnumerable<RecipeFieldDomainEntity> recipeFieldEntities = new[]
+        {
+            CreateFieldEntity(random.Next()),
+            CreateFieldEntity(random.Next()),
+            CreateFieldEntity(random.Next())
+        };
+
+        var recipe = new RecipeDomainEntity(authorEntity, recipeFieldEntities, recipeTitle);
+
+        // Call
+        int totalLength = recipe.TotalLength;
+
+        // Assert
+        int expectedLength = recipeTitle.Length + authorEntity.TotalLength + recipeFieldEntities.Sum(f => f.TotalLength);
+        Assert.Equal(expectedLength, totalLength);
+    }
+
+    [Fact]
+    public void Entity_with_empty_recipe_fields_returns_total_length_of_properties()
+    {
+        // Setup
+        const string recipeTitle = "recipeTitle";
+
+        AuthorDomainEntity authorEntity = CreateAuthorEntity();
+        var recipe = new RecipeDomainEntity(authorEntity, Enumerable.Empty<RecipeFieldDomainEntity>(), recipeTitle);
+
+        // Call
+        int totalLength = recipe.TotalLength;
+
+        // Assert
+        int expectedLength = recipeTitle.Length + authorEntity.TotalLength;
+        Assert.Equal(expectedLength, totalLength);
+    }
+
+    private static AuthorDomainEntity CreateAuthorEntity()
+    {
+        const string authorImageUrl = "http://www.google.com";
+
+        var random = new Random(21);
+        var authorName = new string('o', random.Next(100));
+
+        return new AuthorDomainEntity(authorName, authorImageUrl);
+    }
+
+    private static RecipeFieldDomainEntity CreateFieldEntity(int seed)
+    {
+        var random = new Random(seed);
+        var fieldName = new string('+', random.Next(100));
+        var fieldData = new string('x', random.Next(100));
+
+        return new RecipeFieldDomainEntity(fieldName, fieldData);
+    }
+}
