f60df5114a49df75749252d37b179424b9b039f5
Dennis Tang <karabasf@gmail.com>
2022-08-25 20:08:59 +0200

    Updated all licenseheaders

--- src/Discord.Common/Options/DiscordCommandOptions.cs
+++ src/Discord.Common/Options/DiscordCommandOptions.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
 //
-// This file is part of WeekendBot.
+// This file is part of RecipeBot.
 //
-// WeekendBot is free software: you can redistribute it and/or modify
+// RecipeBot is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
