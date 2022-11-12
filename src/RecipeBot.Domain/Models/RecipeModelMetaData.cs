82d1a4e343248ef15f49ba513a8e4c45f1d05b5a
Dennis Tang <dennis.sht@gmail.com>
2022-11-12 13:42:20 +0100

    Added metadata object as a constructor argument for recipe models

---
+++ src/RecipeBot.Domain/Models/RecipeModelMetaData.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of RecipeBot.
+//
+// RecipeBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.ComponentModel;
+using Common.Utils;
+using RecipeBot.Domain.Data;
+
+namespace RecipeBot.Domain.Models;
+
+/// <summary>
+/// Class containing the metadata of a recipe.
+/// </summary>
+internal class RecipeModelMetaData
+{
+    /// <summary>
+    /// Creates a new instance of a <see cref="RecipeModelMetaData"/>.
+    /// </summary>
+    /// <param name="author">The author information/</param>
+    /// <param name="tags">The tags.</param>
+    /// <param name="category">The <see cref="RecipeCategory"/>.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="author"/> or <paramref name="tags"/> is <c>null</c>.</exception>
+    /// <exception cref="InvalidEnumArgumentException">Thrown when <paramref name="category"/> is an invalid <see cref="RecipeCategory"/>.</exception>
+    public RecipeModelMetaData(AuthorModel author, RecipeTagsModel tags, RecipeCategory category)
+    {
+        author.IsNotNull(nameof(author));
+        tags.IsNotNull(nameof(tags));
+        category.IsValidEnum(nameof(category));
+
+        Author = author;
+        Tags = tags;
+        Category = category;
+    }
+
+    /// <summary>
+    /// Gets the author information.
+    /// </summary>
+    public AuthorModel Author { get; }
+
+    /// <summary>
+    /// Gets the tags belonging to the recipe.
+    /// </summary>
+    public RecipeTagsModel Tags { get; }
+
+    /// <summary>
+    /// Gets the category of the recipe.
+    /// </summary>
+    public RecipeCategory Category { get; }
+}
---
+++ test/RecipeBot.Domain.Test/Models/RecipeModelMetaDataTest.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of RecipeBot.
+//
+// RecipeBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.ComponentModel;
+using System.Linq;
+using AutoFixture;
+using RecipeBot.Domain.Data;
+using RecipeBot.Domain.Models;
+using Xunit;
+
+namespace RecipeBot.Domain.Test.Models;
+
+public class RecipeModelMetaDataTest
+{
+    [Fact]
+    public void Creating_metadata_with_invalid_category_throws_exception()
+    {
+        // Setup
+        var fixture = new Fixture();
+        AuthorModel author = fixture.Build<AuthorModel>()
+                                    .FromFactory<string>(authorName => new AuthorModel(authorName, "http://www.recipeBotImage.com"))
+                                    .Create();
+        RecipeTagsModel tags = fixture.Build<RecipeTagsModel>()
+                                      .FromFactory(() => new RecipeTagsModel(Enumerable.Empty<string>()))
+                                      .Create();
+
+        const RecipeCategory invalidCategory = (RecipeCategory)(-1);
+
+        // Call
+        Action call = () => new RecipeModelMetaData(author, tags, invalidCategory);
+
+        // Assert
+        Assert.Throws<InvalidEnumArgumentException>(call);
+    }
+}
