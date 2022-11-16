5350d046d6d35c00bd47b00f08cf18602000f2f5
Dennis Tang <dennis.sht@gmail.com>
2022-11-16 14:47:44 +0100

    Wired tags as part of the response service

--- test/RecipeBot.Discord.Test/Services/RecipeModalResponseServiceTest.cs
+++ test/RecipeBot.Discord.Test/Services/RecipeModalResponseServiceTest.cs
@@ -17,6 +17,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using AutoFixture;
 using Discord;
 using NSubstitute;
@@ -24,7 +25,6 @@ using RecipeBot.Discord.Data;
 using RecipeBot.Discord.Exceptions;
 using RecipeBot.Discord.Services;
 using RecipeBot.Discord.Views;
-using RecipeBot.Domain.Data;
 using RecipeBot.Domain.Factories;
 using Xunit;
 
@@ -32,12 +32,42 @@ namespace RecipeBot.Discord.Test.Services;
 
 public class RecipeModalResponseServiceTest
 {
+    private static readonly Dictionary<DiscordRecipeCategory, string> categoryMapping = new()
+    {
+        {
+            DiscordRecipeCategory.Meat, "Meat"
+        },
+        {
+            DiscordRecipeCategory.Fish, "Fish"
+        },
+        {
+            DiscordRecipeCategory.Vegetarian, "Vegetarian"
+        },
+        {
+            DiscordRecipeCategory.Vegan, "Vegan"
+        },
+        {
+            DiscordRecipeCategory.Drinks, "Drinks"
+        },
+        {
+            DiscordRecipeCategory.Pastry, "Pastry"
+        },
+        {
+            DiscordRecipeCategory.Dessert, "Dessert"
+        },
+        {
+            DiscordRecipeCategory.Snack, "Snack"
+        },
+        {
+            DiscordRecipeCategory.Other, "Other"
+        }
+    };
 
     [Fact]
     public void Recipe_with_valid_data_and_invalid_category_throws_exception()
     {
         // Setup
-        const DiscordRecipeCategory category = (DiscordRecipeCategory) (-1);
+        const DiscordRecipeCategory category = (DiscordRecipeCategory)(-1);
 
         var user = Substitute.For<IUser>();
         user.Username.Returns("Recipe author");
@@ -48,7 +78,8 @@ public class RecipeModalResponseServiceTest
             RecipeTitle = "Recipe title",
             Ingredients = "My ingredients",
             CookingSteps = "My recipe steps",
-            Notes = "My notes"
+            Notes = "My notes",
+            Tags = "Tag1, Tag2, Tag1"
         };
 
         IRecipeModelCharacterLimitProvider limitProvider = CreateDiscordCharacterLimitProvider();
@@ -81,7 +112,8 @@ public class RecipeModalResponseServiceTest
             RecipeTitle = "Recipe title",
             Ingredients = "My ingredients",
             CookingSteps = "My recipe steps",
-            Notes = "My notes"
+            Notes = "My notes",
+            Tags = "Tag1, Tag2, Tag1"
         };
 
         var limitProvider = Substitute.For<IRecipeModelCharacterLimitProvider>();
@@ -89,6 +121,7 @@ public class RecipeModalResponseServiceTest
         limitProvider.MaximumAuthorNameLength.Returns(EmbedAuthorBuilder.MaxAuthorNameLength);
         limitProvider.MaximumFieldNameLength.Returns(EmbedFieldBuilder.MaxFieldNameLength);
         limitProvider.MaximumFieldDataLength.Returns(EmbedFieldBuilder.MaxFieldValueLength);
+        limitProvider.MaximumRecipeTagsLength.Returns(EmbedFooterBuilder.MaxFooterTextLength);
 
         var service = new RecipeModalResponseService(limitProvider);
 
@@ -113,28 +146,29 @@ public class RecipeModalResponseServiceTest
         var user = Substitute.For<IUser>();
         user.Username.Returns(authorName);
         user.GetAvatarUrl().ReturnsForAnyArgs(authorImageUrl);
-    
+
         const string recipeTitle = "Recipe title";
         const string recipeIngredients = "My ingredients";
         const string recipeSteps = "My recipe steps";
         const string recipeNotes = "My notes";
-    
+        const string tags = "Tag1, Tag2, Tag1";
         var modal = new RecipeModal
         {
             RecipeTitle = recipeTitle,
             Ingredients = recipeIngredients,
             CookingSteps = recipeSteps,
-            Notes = recipeNotes
+            Notes = recipeNotes,
+            Tags = tags
         };
-    
+
         IRecipeModelCharacterLimitProvider limitProvider = CreateDiscordCharacterLimitProvider();
         var service = new RecipeModalResponseService(limitProvider);
-    
+
         // Call
         Embed response = service.GetRecipeModalResponse(modal, user, category);
-    
+
         // Assert
-        AssertCommonEmbedResponseProperties(user, modal, expectedColor, response);
+        AssertCommonEmbedResponseProperties(user, category, modal, expectedColor, response);
         Assert.Null(response.Image);
     }
 
@@ -158,7 +192,8 @@ public class RecipeModalResponseServiceTest
             RecipeTitle = "Recipe title",
             Ingredients = "My ingredients",
             CookingSteps = "My recipe steps",
-            Notes = "My notes"
+            Notes = "My notes",
+            Tags = "tag1, tag2"
         };
 
         var limitProvider = Substitute.For<IRecipeModelCharacterLimitProvider>();
@@ -167,6 +202,7 @@ public class RecipeModalResponseServiceTest
         limitProvider.MaximumAuthorNameLength.Returns(EmbedAuthorBuilder.MaxAuthorNameLength);
         limitProvider.MaximumFieldNameLength.Returns(EmbedFieldBuilder.MaxFieldNameLength);
         limitProvider.MaximumFieldDataLength.Returns(EmbedFieldBuilder.MaxFieldValueLength);
+        limitProvider.MaximumRecipeTagsLength.Returns(EmbedFooterBuilder.MaxFooterTextLength);
 
         var service = new RecipeModalResponseService(limitProvider);
 
@@ -184,7 +220,7 @@ public class RecipeModalResponseServiceTest
     public void Recipe_with_valid_attachment_and_valid_data_and_invalid_category_throws_exception()
     {
         // Setup
-        const DiscordRecipeCategory category = (DiscordRecipeCategory) (-1);
+        const DiscordRecipeCategory category = (DiscordRecipeCategory)(-1);
 
         var user = Substitute.For<IUser>();
         user.Username.Returns("Recipe author");
@@ -199,10 +235,11 @@ public class RecipeModalResponseServiceTest
             RecipeTitle = "Recipe title",
             Ingredients = "My ingredients",
             CookingSteps = "My recipe steps",
-            Notes = "My notes"
+            Notes = "My notes",
+            Tags = "Tag1, Tag2"
         };
 
-        var limitProvider = CreateDiscordCharacterLimitProvider();
+        IRecipeModelCharacterLimitProvider limitProvider = CreateDiscordCharacterLimitProvider();
 
         var service = new RecipeModalResponseService(limitProvider);
 
@@ -237,12 +274,14 @@ public class RecipeModalResponseServiceTest
         const string recipeIngredients = "My ingredients";
         const string recipeSteps = "My recipe steps";
         const string recipeNotes = "My notes";
+        const string tags = "Tag1, Tag2, Tag1";
         var modal = new RecipeModal
         {
             RecipeTitle = recipeTitle,
             Ingredients = recipeIngredients,
             CookingSteps = recipeSteps,
-            Notes = recipeNotes
+            Notes = recipeNotes,
+            Tags = tags
         };
 
         IRecipeModelCharacterLimitProvider limitProvider = CreateDiscordCharacterLimitProvider();
@@ -252,53 +291,12 @@ public class RecipeModalResponseServiceTest
         Embed response = service.GetRecipeModalResponse(modal, user, category, attachment);
 
         // Assert
-        AssertCommonEmbedResponseProperties(user, modal, expectedColor, response);
+        AssertCommonEmbedResponseProperties(user, category, modal, expectedColor, response);
 
         EmbedImage? embedImage = response.Image;
         Assert.NotNull(embedImage);
         EmbedImage resultImage = embedImage!.Value;
         Assert.Equal(recipeImageUrl, resultImage.Url);
-    }
-
-    private static IRecipeModelCharacterLimitProvider CreateDiscordCharacterLimitProvider()
-    {
-        var limitProvider = Substitute.For<IRecipeModelCharacterLimitProvider>();
-        limitProvider.MaximumTitleLength.Returns(EmbedBuilder.MaxTitleLength);
-        limitProvider.MaximumRecipeLength.Returns(EmbedBuilder.MaxEmbedLength);
-        limitProvider.MaximumAuthorNameLength.Returns(EmbedAuthorBuilder.MaxAuthorNameLength);
-        limitProvider.MaximumFieldNameLength.Returns(EmbedFieldBuilder.MaxFieldNameLength);
-        limitProvider.MaximumFieldDataLength.Returns(EmbedFieldBuilder.MaxFieldValueLength);
-        return limitProvider;
-    }
-
-    private static void AssertCommonEmbedResponseProperties(IUser user, RecipeModal modal, Color color, IEmbed actualResponse)
-    {
-        EmbedAuthor? actualResponseAuthor = actualResponse.Author;
-        Assert.NotNull(actualResponseAuthor);
-        EmbedAuthor embedAuthor = actualResponseAuthor!.Value;
-        AssertAuthor(user.Username, user.GetAvatarUrl(), embedAuthor);
-
-        Assert.Equal(modal.RecipeTitle, actualResponse.Title);
-
-        Assert.Equal(3, actualResponse.Fields.Length);
-        AssertField("Ingredients", modal.Ingredients, actualResponse.Fields[0]);
-        AssertField("Cooking steps", modal.CookingSteps, actualResponse.Fields[1]);
-        AssertField("Additional notes", modal.Notes, actualResponse.Fields[2]);
-
-        Assert.Equal(color, actualResponse.Color);
-    }
-
-    private static void AssertAuthor(string expectedAuthorName, string expectedAuthorImageUrl, EmbedAuthor actualAuthor)
-    {
-        Assert.Equal(expectedAuthorName, actualAuthor.Name);
-        Assert.Equal(expectedAuthorImageUrl, actualAuthor.IconUrl);
-    }
-
-    private static void AssertField(string expectedName, string? expectedValue, EmbedField actualField)
-    {
-        Assert.Equal(expectedName, actualField.Name);
-        Assert.Equal(expectedValue, actualField.Value);
-        Assert.False(actualField.Inline);
     }
 
     public static IEnumerable<object[]> GetRecipeCategoriesAndColor()
@@ -350,5 +348,68 @@ public class RecipeModalResponseServiceTest
             DiscordRecipeCategory.Other,
             new Color(165, 161, 164)
         };
+    }
+
+    private static IRecipeModelCharacterLimitProvider CreateDiscordCharacterLimitProvider()
+    {
+        var limitProvider = Substitute.For<IRecipeModelCharacterLimitProvider>();
+        limitProvider.MaximumTitleLength.Returns(EmbedBuilder.MaxTitleLength);
+        limitProvider.MaximumRecipeLength.Returns(EmbedBuilder.MaxEmbedLength);
+        limitProvider.MaximumAuthorNameLength.Returns(EmbedAuthorBuilder.MaxAuthorNameLength);
+        limitProvider.MaximumFieldNameLength.Returns(EmbedFieldBuilder.MaxFieldNameLength);
+        limitProvider.MaximumFieldDataLength.Returns(EmbedFieldBuilder.MaxFieldValueLength);
+        limitProvider.MaximumRecipeTagsLength.Returns(EmbedFooterBuilder.MaxFooterTextLength);
+        return limitProvider;
+    }
+
+    private static void AssertCommonEmbedResponseProperties(
+        IUser user, DiscordRecipeCategory category, RecipeModal modal, Color color, IEmbed actualResponse)
+    {
+        EmbedAuthor? actualResponseAuthor = actualResponse.Author;
+        Assert.NotNull(actualResponseAuthor);
+        AssertAuthor(user.Username, user.GetAvatarUrl(), actualResponseAuthor.Value);
+
+        Assert.Equal(modal.RecipeTitle, actualResponse.Title);
+
+        Assert.Equal(3, actualResponse.Fields.Length);
+        AssertField("Ingredients", modal.Ingredients, actualResponse.Fields[0]);
+        AssertField("Cooking steps", modal.CookingSteps, actualResponse.Fields[1]);
+        AssertField("Additional notes", modal.Notes, actualResponse.Fields[2]);
+
+        Assert.Equal(color, actualResponse.Color);
+
+        EmbedFooter? actualResponseFooter = actualResponse.Footer;
+        Assert.NotNull(actualResponseFooter);
+        AssertTags(category, modal.Tags, actualResponseFooter.Value);
+    }
+
+    private static void AssertAuthor(string expectedAuthorName, string expectedAuthorImageUrl, EmbedAuthor actualAuthor)
+    {
+        Assert.Equal(expectedAuthorName, actualAuthor.Name);
+        Assert.Equal(expectedAuthorImageUrl, actualAuthor.IconUrl);
+    }
+
+    private static void AssertField(string expectedName, string? expectedValue, EmbedField actualField)
+    {
+        Assert.Equal(expectedName, actualField.Name);
+        Assert.Equal(expectedValue, actualField.Value);
+        Assert.False(actualField.Inline);
+    }
+
+    private static void AssertTags(DiscordRecipeCategory category, string tags, EmbedFooter actualFooter)
+    {
+        var expectedTags = new List<string>
+        {
+            categoryMapping[category]
+        };
+
+        if (!string.IsNullOrWhiteSpace(tags))
+        {
+            IEnumerable<string> parsedTags = tags.Split(",").Select(t => t.Trim()).Distinct();
+            expectedTags.AddRange(parsedTags);
+        }
+
+        string expectedFooterText = string.Join(", ", expectedTags);
+        Assert.Equal(expectedFooterText, actualFooter.Text);
     }
 }
