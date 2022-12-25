dcde811eef6a73d81df5e1da5e854af34d78678c
Dennis Tang <dennis.sht@gmail.com>
2022-12-25 11:20:58 +0100

    Added entities to test structure

---
+++ src/RecipeBot.Persistence/Entities/RecipeEntity.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of RecipeBot.
+//
+// RecipeBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+namespace RecipeBot.Persistence.Entities;
+
+public class RecipeEntity
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public AuthorEntity Author { get; set; }
+
+    public int AuthorId { get; set; }
+}
