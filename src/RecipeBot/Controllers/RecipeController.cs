2c64afe618c7b7e8883ddbb93dfbe5a7335de4d0
Dennis Tang <dennis.sht@gmail.com>
2023-01-05 12:22:07 +0100

    Added initial setup of the RecipeController

---
+++ src/RecipeBot/Controllers/RecipeController.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of RecipeBot.
+//
+// RecipeBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Threading.Tasks;
+using Common.Utils;
+using Discord;
+using RecipeBot.Discord.Controllers;
+using RecipeBot.Discord.Data;
+using RecipeBot.Discord.Views;
+using RecipeBot.Domain.Factories;
+
+namespace RecipeBot.Controllers;
+
+/// <summary>
+/// A concrete implementation of the <see cref="IRecipeController"/>.
+/// </summary>
+public class RecipeController : IRecipeController
+{
+    private readonly RecipeModelFactory factory;
+
+    public RecipeController(RecipeModelFactory factory)
+    {
+        this.factory = factory;
+        factory.IsNotNull(nameof(factory));
+    }
+
+    public Task<ControllerResult<Embed>> SaveRecipeAsync(RecipeModal modal, IUser user,
+                                                         DiscordRecipeCategory category, IAttachment attachment)
+    {
+        modal.IsNotNull(nameof(modal));
+        user.IsNotNull(nameof(user));
+        category.IsValidEnum(nameof(category));
+
+        throw new NotImplementedException();
+    }
+}
