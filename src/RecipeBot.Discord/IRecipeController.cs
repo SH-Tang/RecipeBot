ef3590a33bfae7874ee98c58f869c8b1823c76ae
Dennis Tang <dennis.sht@gmail.com>
2023-01-05 11:57:46 +0100

    Introduced interface to describe the recipe controller
    - Controller is to be injected in the interaction modules

---
+++ src/RecipeBot.Discord/IRecipeController.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of RecipeBot.
+//
+// RecipeBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using Discord;
+using RecipeBot.Discord.Data;
+using RecipeBot.Discord.Views;
+
+namespace RecipeBot.Discord;
+
+/// <summary>
+/// Interface for describing a controller dealing with recipe interactions.
+/// </summary>
+public interface IRecipeController
+{
+    /// <summary>
+    /// Saves a recipe based its input arguments.
+    /// </summary>
+    /// <param name="modal">The <see cref="RecipeModal"/> to get the data from.</param>
+    /// <param name="user">The <see cref="IUser"/> invoking the command.</param>
+    /// <param name="category">The <see cref="DiscordRecipeCategory"/> the recipe belongs to.</param>
+    /// <param name="attachment">The <see cref="IAttachment"/> that belongs to the recipe.</param>
+    /// <returns>The saved data from the <see cref="Embed"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="modal"/> and <paramref name="user"/> is <c>null</c>.</exception>
+    Embed SaveRecipe(RecipeModal modal, IUser user, DiscordRecipeCategory category, IAttachment attachment);
+}
