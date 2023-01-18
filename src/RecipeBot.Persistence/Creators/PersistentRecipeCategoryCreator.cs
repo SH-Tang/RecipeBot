01cc436a09e1485ed1af6fc79e7b3ad19290f7a4
Dennis Tang <dennis.sht@gmail.com>
2023-01-18 17:23:54 +0100

    Introduced repository for data entry collections

---
+++ src/RecipeBot.Persistence/Creators/PersistentRecipeCategoryCreator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using Common.Utils;
+using RecipeBot.Domain.Data;
+using RecipeBot.Persistence.Entities;
+
+namespace RecipeBot.Persistence.Creators;
+
+/// <summary>
+/// Creator to create <see cref="PersistentRecipeCategory"/>.
+/// </summary>
+internal static class PersistentRecipeCategoryCreator
+{
+    /// <summary>
+    /// Creates a <see cref="PersistentRecipeCategory"/> based on its input argument.
+    /// </summary>
+    /// <param name="category">The <see cref="RecipeCategory"/> to create the <see cref="PersistentRecipeCategory"/> with.</param>
+    /// <returns>A <see cref="PersistentRecipeCategory"/>.</returns>
+    /// <exception cref="InvalidEnumArgumentException">Thrown when <paramref name="category"/> is an invalid <see cref="RecipeCategory"/>.</exception>
+    /// <exception cref="NotSupportedException">Thrown when <paramref name="category"/> is a valid <see cref="RecipeCategory"/>,
+    /// but unsupported.</exception>
+    public static PersistentRecipeCategory Create(RecipeCategory category)
+    {
+        category.IsValidEnum(nameof(category));
+
+        switch (category)
+        {
+            case RecipeCategory.Meat:
+                return PersistentRecipeCategory.Meat;
+            case RecipeCategory.Fish:
+                return PersistentRecipeCategory.Fish;
+            case RecipeCategory.Vegetarian:
+                return PersistentRecipeCategory.Vegetarian;
+            case RecipeCategory.Vegan:
+                return PersistentRecipeCategory.Vegan;
+            case RecipeCategory.Drinks:
+                return PersistentRecipeCategory.Drinks;
+            case RecipeCategory.Pastry:
+                return PersistentRecipeCategory.Pastry;
+            case RecipeCategory.Dessert:
+                return PersistentRecipeCategory.Dessert;
+            case RecipeCategory.Snack:
+                return PersistentRecipeCategory.Snack;
+            case RecipeCategory.Other:
+                return PersistentRecipeCategory.Other;
+            default:
+                throw new NotSupportedException();
+        }
+    }
+}
---
+++ src/RecipeBot.Persistence/RecipeDataEntryCollectionRepository.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of RecipeBot.
+//
+// RecipeBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Utils;
+using Microsoft.EntityFrameworkCore;
+using RecipeBot.Domain.Data;
+using RecipeBot.Domain.Repositories;
+using RecipeBot.Domain.Repositories.Data;
+using RecipeBot.Persistence.Creators;
+using RecipeBot.Persistence.Entities;
+
+namespace RecipeBot.Persistence;
+
+/// <summary>
+/// An EF Core implementation of the <see cref="IRecipeDataEntryCollectionRepository"/>.
+/// </summary>
+public class RecipeDataEntryCollectionRepository : IRecipeDataEntryCollectionRepository
+{
+    private readonly RecipeBotDbContext context;
+
+    /// <summary>
+    /// Creates a new instance of <see cref="RecipeDataEntryCollectionRepository"/>.
+    /// </summary>
+    /// <param name="context">The <see cref="RecipeBotDbContext"/>.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="context"/> is <c>null</c>.</exception>
+    public RecipeDataEntryCollectionRepository(RecipeBotDbContext context)
+    {
+        context.IsNotNull(nameof(context));
+
+        this.context = context;
+    }
+
+    public async Task<IReadOnlyList<RecipeEntryData>> LoadRecipeEntriesAsync()
+    {
+        return await context.RecipeEntities
+                            .Include(r => r.Author)
+                            .Select(r => new RecipeEntryData(r.RecipeEntityId, r.RecipeTitle, r.Author.AuthorName))
+                            .OrderBy(r => r.Id)
+                            .ToArrayAsync();
+    }
+
+    public async Task<IReadOnlyList<RecipeEntryData>> LoadRecipeEntriesAsync(RecipeCategory category)
+    {
+        PersistentRecipeCategory persistentCategory = PersistentRecipeCategoryCreator.Create(category);
+
+        return await context.RecipeEntities
+                            .Include(r => r.Author)
+                            .Where(r => r.RecipeCategory == persistentCategory)
+                            .Select(r => new RecipeEntryData(r.RecipeEntityId, r.RecipeTitle, r.Author.AuthorName))
+                            .OrderBy(r => r.Id)
+                            .ToArrayAsync();
+    }
+}
