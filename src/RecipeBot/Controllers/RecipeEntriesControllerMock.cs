d5f246d23ec155fce8d0d5df0312fbd52ece6e61
Dennis Tang <dennis.sht@gmail.com>
2023-01-28 11:45:06 +0100

    Added initial implementation of the controller:
    - Added message length provider

---
+++ src/RecipeBot/Controllers/RecipeEntriesControllerMock.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of RecipeBot.
+//
+// RecipeBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Utils;
+using Discord;
+using Discord.Common;
+using RecipeBot.Discord.Controllers;
+using RecipeBot.Discord.Data;
+using RecipeBot.Domain.Repositories;
+using RecipeBot.Domain.Repositories.Data;
+
+namespace RecipeBot.Controllers;
+
+/// <summary>
+/// A concrete implementation of the <see cref="IRecipeEntriesController"/>.
+/// </summary>
+public class RecipeEntriesControllerMock : IRecipeEntriesController
+{
+    private readonly IMessageCharacterLimitProvider limitProvider;
+    private readonly IRecipeDataEntryCollectionRepository repository;
+    private readonly ILoggingService logger;
+
+    /// <summary>
+    /// Creates a new instance of <see cref="RecipeEntriesControllerMock"/>.
+    /// </summary>
+    /// <param name="limitProvider">The limit provider to retrieve the message character limits from.</param>
+    /// <param name="repository">The repository to handle with the persistence of recipe entries.</param>
+    /// <param name="logger">The logger to log with.</param>
+    /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+    public RecipeEntriesControllerMock(IMessageCharacterLimitProvider limitProvider, 
+        IRecipeDataEntryCollectionRepository repository, ILoggingService logger)
+    {
+        limitProvider.IsNotNull(nameof(limitProvider));
+        repository.IsNotNull(nameof(repository));
+        logger.IsNotNull(nameof(logger));
+
+        this.limitProvider = limitProvider;
+        this.repository = repository;
+        this.logger = logger;
+    }
+
+    public async Task<ControllerResult<IReadOnlyList<string>>> ListAllRecipesAsync()
+    {
+        IReadOnlyList<RecipeEntryData> entries = await repository.LoadRecipeEntriesAsync();
+        if (!entries.Any())
+        {
+            return new ControllerResult<IReadOnlyList<string>>(new[]
+            {
+                "No saved recipes are found."
+            });
+        }
+
+        var header = $"{"Id",-3} {"Title",-50} {"Author",-50} ";
+        string formattedEntries = string.Join($"{Environment.NewLine}", entries.Select(r => $"{r.Id,-3} {r.Title,-50} {r.AuthorName,-50}"));
+
+        return new ControllerResult<IReadOnlyList<string>>(new []
+        {
+            Format.Code($"{header}{Environment.NewLine}{formattedEntries}")
+        });
+    }
+
+    public Task<ControllerResult<IReadOnlyList<string>>> ListAllRecipesAsync(DiscordRecipeCategory category)
+    {
+        category.IsValidEnum(nameof(category));
+
+        return Task.FromResult(new ControllerResult<IReadOnlyList<string>>("Not implemented yet"));
+    }
+}
---
+++ test/RecipeBot.Test/Controllers/RecipeEntriesControllerTest.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of RecipeBot.
+//
+// RecipeBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using Discord;
+using Discord.Common;
+using FluentAssertions;
+using NSubstitute;
+using RecipeBot.Controllers;
+using RecipeBot.Discord.Controllers;
+using RecipeBot.Domain.Factories;
+using RecipeBot.Domain.Repositories;
+using RecipeBot.Domain.Repositories.Data;
+using Xunit;
+
+namespace RecipeBot.Test.Controllers;
+
+public class RecipeEntriesControllerTest
+{
+    [Fact]
+    public void Controller_is_recipe_entries_controller()
+    {
+        // Setup
+        var limitProvider = Substitute.For<IMessageCharacterLimitProvider>();
+        var repository = Substitute.For<IRecipeDataEntryCollectionRepository>();
+        var logger = Substitute.For<ILoggingService>();
+
+        // Call
+        var controller = new RecipeEntriesControllerMock(limitProvider, repository, logger);
+
+        // Assert
+        controller.Should().BeAssignableTo<IRecipeEntriesController>();
+    }
+
+    [Fact]
+    public async Task Given_repository_returning_empty_collection_when_listing_all_recipes_returns_expected_message()
+    {
+        // Setup
+        var limitProvider = Substitute.For<IMessageCharacterLimitProvider>();
+        var logger = Substitute.For<ILoggingService>();
+
+        var repository = Substitute.For<IRecipeDataEntryCollectionRepository>();
+        repository.LoadRecipeEntriesAsync().ReturnsForAnyArgs(Array.Empty<RecipeEntryData>());
+
+        var controller = new RecipeEntriesControllerMock(limitProvider, repository, logger);
+
+        // Call
+        ControllerResult<IReadOnlyList<string>> result = await controller.ListAllRecipesAsync();
+
+        // Assert
+        result.HasError.Should().BeFalse();
+
+        result.Result.Should().HaveCount(1).And.Contain("No saved recipes are found.");
+    }
+
+    [Fact]
+    public async Task Given_repository_returning_collection_and_message_within_limits_when_listing_all_recipes_returns_expected_message()
+    {
+        // Setup
+        var logger = Substitute.For<ILoggingService>();
+
+        var limitProvider = Substitute.For<IMessageCharacterLimitProvider>();
+        limitProvider.MaxMessageLength.Returns(int.MaxValue);
+
+        var fixture = new Fixture();
+        RecipeEntryData[] entries = fixture.CreateMany<RecipeEntryData>(3).ToArray();
+
+        var repository = Substitute.For<IRecipeDataEntryCollectionRepository>();
+        repository.LoadRecipeEntriesAsync().ReturnsForAnyArgs(entries);
+
+        var controller = new RecipeEntriesControllerMock(limitProvider, repository, logger);
+
+        // Call
+        ControllerResult<IReadOnlyList<string>> result = await controller.ListAllRecipesAsync();
+
+        // Assert
+        result.HasError.Should().BeFalse();
+
+        string expectedMessage =
+            $"{"Id",-3} {"Title",-50} {"Author",-50} {Environment.NewLine}" +
+            $"{entries[0].Id,-3} {entries[0].Title,-50} {entries[0].AuthorName,-50}{Environment.NewLine}" +
+            $"{entries[1].Id,-3} {entries[1].Title,-50} {entries[1].AuthorName,-50}{Environment.NewLine}" +
+            $"{entries[2].Id,-3} {entries[2].Title,-50} {entries[2].AuthorName,-50}";
+        result.Result.Should().HaveCount(1).And.Contain(Format.Code(expectedMessage));
+    }
+
+}
