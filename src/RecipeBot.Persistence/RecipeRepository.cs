80cd4ed2664313324b17ad84ca3141cdc1b4b231
S.H. Tang <karabasf@gmail.com>
2023-02-08 21:29:47 +0100

    Incorporated comments review:
    - Formatting

--- src/RecipeBot.Persistence/RecipeRepository.cs
+++ src/RecipeBot.Persistence/RecipeRepository.cs
@@ -132,7 +132,8 @@ public class RecipeRepository : IRecipeRepository
 
         return new RecipeTagEntity
         {
-            Tag = tagEntity, Order = i
+            Tag = tagEntity, 
+            Order = i
         };
     }
 
