a6df120ee45ace876a458c1026b8c129c4ed396c
S.H. Tang <dennis.sht@gmail.com>
2023-05-27 12:29:56 +0200

    Update src/Discord.Common/InfoModule/InfoInteractionModule.cs

--- src/Discord.Common/InfoModule/InfoInteractionModule.cs
+++ src/Discord.Common/InfoModule/InfoInteractionModule.cs
@@ -34,7 +34,7 @@ public class InfoInteractionModule : InteractionModuleBase<SocketInteractionCont
     /// <summary>
     /// Creates a new instance of <see cref="InfoInteractionModule"/>.
     /// </summary>
-    /// <param name="controller">The <see cref="IDiscordBotInformationController"/> to coordinate the interactions.</param>
+    /// <param name="controller">The <see cref="IDiscordBotInformationController"/> to coordinate the interactions with.</param>
     /// <param name="logger">The logger to use.</param>
     /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
     public InfoInteractionModule(IDiscordBotInformationController controller, ILoggingService logger)
