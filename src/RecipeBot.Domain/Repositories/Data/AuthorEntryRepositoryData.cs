e3b777930ff54b8e5f3bfb0cfefc8304073659b4
S.H. Tang <karabasf@gmail.com>
2023-07-01 14:12:51 +0200

    Added data class to transfer information of the author in the repository

---
+++ src/RecipeBot.Domain/Repositories/Data/AuthorEntryRepositoryData.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of RecipeBot.
+//
+// RecipeBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+namespace RecipeBot.Domain.Repositories.Data;
+
+/// <summary>
+/// Class representing an author entry in a repository.
+/// </summary>
+public class AuthorEntryRepositoryData
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="AuthorEntryRepositoryData"/>.
+    /// </summary>
+    /// <param name="entityId">The id of the author as present in the repository.</param>
+    /// <param name="authorId">The id of the author.</param>
+    public AuthorEntryRepositoryData(int entityId, ulong authorId)
+    {
+        EntityId = entityId;
+        AuthorId = authorId;
+    }
+
+    /// <summary>
+    /// Gets the id of the author as present in the repository.
+    /// </summary>
+    public int EntityId { get; }
+
+    /// <summary>
+    /// Gets the id of the author.
+    /// </summary>
+    public ulong AuthorId { get; }
+}
