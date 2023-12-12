e45d51c97fa4aca21dd9948f43a41cbd27432e3a
S.H. Tang <karabasf@gmail.com>
2023-12-12 21:18:42 +0100

    Added service to parse web recipe meta data

---
+++ src/RecipeBot/Services/ParsedWebRecipe.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of RecipeBot.
+//
+// RecipeBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+namespace RecipeBot.Services
+{
+    /// <summary>
+    /// Class containing the parsed web recipe data.
+    /// </summary>
+    internal class ParsedWebRecipe
+    {
+        /// <summary>
+        /// Gets or  sets the title.
+        /// </summary>
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the short name of the website.
+        /// </summary>
+        public string? SiteName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the image url.
+        /// </summary>
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ src/RecipeBot/Services/WebRecipeParsingService.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (C) 2022 Dennis Tang. All rights reserved.
+//
+// This file is part of RecipeBot.
+//
+// RecipeBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+using Common.Utils;
+using HtmlAgilityPack;
+
+namespace RecipeBot.Services;
+
+/// <summary>
+/// Service responsible for parsing web recipes.
+/// </summary>
+internal class WebRecipeParsingService
+{
+    private const string xPathTitle = "//meta[@property='og:title']/@content";
+    private const string xPathDescription = "//meta[@property='og:description']/@content";
+    private const string xPathShortSiteName = "//meta[@property='og:site_name']/@content";
+    private const string xPathImage = "//meta[@property='og:image']/@content";
+
+    private readonly IHtmlContentProvider provider;
+
+    /// <summary>
+    /// Creates a new instance.
+    /// </summary>
+    /// <param name="provider">The <see cref="IHtmlContentProvider"/> to retrieve the web content with.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="provider"/> is <c>null</c>.</exception>
+    public WebRecipeParsingService(IHtmlContentProvider provider)
+    {
+        provider.IsNotNull(nameof(provider));
+
+        this.provider = provider;
+    }
+
+    public async Task<ParsedWebRecipe> GetParsedWebRecipe(string webRecipeUrl)
+    {
+        // Wrap this in a custom exception
+        string content = await provider.GetHtmlContent(webRecipeUrl);
+
+        var doc = new HtmlDocument();
+        doc.LoadHtml(content);
+
+        XPathNavigator navigator = doc.CreateNavigator() ?? throw new InvalidOperationException();
+
+        return new ParsedWebRecipe
+        {
+            Title = navigator.SelectSingleNode(xPathTitle)?.Value,
+            Description = navigator.SelectSingleNode(xPathDescription)?.Value,
+            SiteName = navigator.SelectSingleNode(xPathShortSiteName)?.Value,
+            ImageUrl = navigator.SelectSingleNode(xPathImage)?.Value,
+        };
+    }
+}
